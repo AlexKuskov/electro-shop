b46afd30a443f99effd08d916af479cc2d5665f3
FalseNumber <sasha.sanya.aleksandr@gmail.com>
2019-06-21 20:01:54 +0300

    Updated Angular to latest version. Added header, search-bar and languages components. Changed favicon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SearchBarComponent } from './components/header/search-bar/search-bar.component';
+import { LanguagesComponent } from './components/header/languages/languages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchBarComponent,
+    LanguagesComponent
   ],
   imports: [
     BrowserModule,
