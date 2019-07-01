6bfaba9b946e4d7bf13b93329de21e65bce6a241
FalseNumber <sasha.sanya.aleksandr@gmail.com>
2019-07-01 17:37:38 +0300

    Added a few routes in AppRoutingModule, bound them to some html elements

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SearchResultComponent } from './components/content/search-result/search-result.component';
+import { CategoryContentComponent } from './components/content/category-content/category-content.component';
+import { MainComponent } from './components/content/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'search', component: SearchResultComponent },
+  { path: 'category', component: CategoryContentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
