2f54b4c4de80e2897a5bffb0006962a23b5b8d56
FalseNumber <sasha.sanya.aleksandr@gmail.com>
2019-07-15 17:45:03 +0300

    Add switch to empty cart if there are no Cart Items. Add adding/subtracting of cart itemamount

--- src/app/components/modal/cart/cart.component.ts
+++ src/app/components/modal/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from 'src/app/services/cart.service';
+import { CartItem } from 'src/app/model/cart-item';
 
 @Component({
   selector: 'app-cart',
@@ -11,6 +12,16 @@ export class CartComponent implements OnInit {
   constructor(public cartService: CartService) { }
 
   ngOnInit() {
+  }
+
+  addItemAmount(cartItem: CartItem) {
+    cartItem.amount++;
+    this.cartService.updateTotalPrice();
+  }
+
+  subtractItemAmount(cartItem: CartItem) {
+    if (cartItem.amount > 1) cartItem.amount--;
+    this.cartService.updateTotalPrice();
   }
 
 }
